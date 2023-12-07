6e1cd723b9b92b84ce53b362bf1f9cbb2f3c74d0
AniModi <98632268+AniModi@users.noreply.github.com>
2023-12-07 13:17:24 +0530

    Refactor result upload controller to include student verification

--- controllers/resultUploadController.js
+++ controllers/resultUploadController.js
@@ -1,8 +1,10 @@
 const StudentDocument = require("../models/StudentDocument");
+const StudentVerification = require("../models/StudentVerifications");
 
 async function uploadResult(req, res) {
   try {
-    const { username, semester, hash } = req.body;
+    console.log("uploadResult", req.body);
+    const { username, semester, result } = req.body;
     let studentDocuments = await StudentDocument.findOne({
       username: username,
       semester: semester,
@@ -16,30 +18,43 @@ async function uploadResult(req, res) {
         },
         {
           $set: {
-            result: hash,
+            result: result,
           },
         },
         {
           new: true,
         }
       );
+      const verifications = await StudentVerification.findOneAndUpdate({
+        username: username,
+        semester: semester,
+      }, {
+        $set: {
+          resultPublished: true
+        }
+      }, {
+        new: true
+      });
       res.status(200).json({
         message: "Result uploaded successfully",
+        data : studentDocuments
       });
     } else {
       const newStudentDocument = new StudentDocument({
         username: username,
         semester: semester,
-        result: hash,
+        result: result,
       });
       await newStudentDocument.save();
       res.status(200).json({
         message: "Result uploaded successfully",
+        data : newStudentDocument
       });
     }
   } catch (err) {
     res.status(500).json({
       message: "Internal server error",
+      error: err,
     });
   }
 }
